bf6f5c95a93662009c34da8584d9ec350cc077a9
Yimien <42413155+Yimien@users.noreply.github.com>
2024-05-28 13:58:04 +0800

    增强插件配置检查

--- src/controllers/plugin/config.ts
+++ src/controllers/plugin/config.ts
@@ -1,28 +1,28 @@
 import {IItemCondition} from "@/types/plugin";
-import {syncPlanKey, versionKey} from "@/constants/components/select";
+import {syncPlanKey} from "@/constants/components/select";
 import {isEmptyValue} from "@/utils";
 import {pluginConfigData} from "@/index";
+import {lsNotebooks} from "@/controllers/siyuan/api";
 
 export class PlugConfig {
     /**
      * 检查插件配置是否正确
      */
     static async check() {
+
         const items = [
             pluginConfigData.base.version,
             pluginConfigData.base.host,
             pluginConfigData.base.token,
             pluginConfigData.base.syncPlan,
             pluginConfigData.base.notebook,
-            pluginConfigData.base.lastSyncTime,
 
-            pluginConfigData.advanced.labelMatch,
             pluginConfigData.advanced.isHandleBacklinks,
             pluginConfigData.advanced.isSuperLabel,
-            pluginConfigData.advanced.quoteHandle,
             pluginConfigData.advanced.isHandleVideo,
             pluginConfigData.advanced.isHandleHref,
-            pluginConfigData.advanced.imageBlockLayout,
+
+            pluginConfigData.filter.lastSyncTime,
 
             pluginConfigData.debug.isDebug,
             pluginConfigData.debug.isAutoUpdateTime
@@ -40,25 +40,26 @@ export class PlugConfig {
             {
                 flag: !(isEmptyValue(pluginConfigData.advanced.isHandleVideo)),
                 value: pluginConfigData.advanced.videoFormats
-            },
-            {
-                flag: pluginConfigData.base.version === versionKey.v1,
-                value: pluginConfigData.filter.resourceDownload
             }
         ]
 
+        // 判断必填项是否存在
         for (const item of items) {
             if (isEmptyValue(item)) {
                 return false;
             }
         }
 
+        // 判断某种条件的必填项是否存在
         for (const flag in itemsConditions) {
             if (flag && (isEmptyValue(items[flag]))) {
                 return false;
             }
         }
 
-        return true;
+        // 判断同步笔记本是否存在
+        let notebooks = await lsNotebooks();
+        let notebookIdList: string[] = notebooks.notebooks.map(notebook => notebook.id);
+        return notebookIdList.includes(pluginConfigData.base.notebook);
     }
 }
