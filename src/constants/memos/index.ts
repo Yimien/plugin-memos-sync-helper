5547577ab3bda2cc05051a231569163443910eaa
Yimien <qq250764195@gmail.com>
2025-02-21 20:18:26 +0800

    修复标签过滤失效的问题

--- src/constants/memos/index.ts
+++ src/constants/memos/index.ts
@@ -3,7 +3,10 @@ import {versionKey} from "@/constants/components/select";
 export const API_VERSION = {
     V1: [versionKey.v0_21_0],
     V2: [versionKey.v0_22_0, versionKey.v0_22_1, versionKey.v0_23_0, versionKey.v0_24_0],
-    V2_LabelFilter: [versionKey.v0_22_1, versionKey.v0_23_0],
+    /**
+     * 支持标签过滤的版本
+     */
+    V2_LabelFilter: [versionKey.v0_22_1, versionKey.v0_23_0, versionKey.v0_24_0],
     V2_DownloadResourceByName: [versionKey.v0_22_0],
     V2_MemosViewFull: [versionKey.v0_23_0],
     /**
